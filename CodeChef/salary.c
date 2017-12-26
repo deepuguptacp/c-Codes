5bcb63721c614964231ad8a190b9f40ffd320465
deepuguptacp <199deepu@gmail.com>
2017-12-26 23:52:23 +0530

    salary

--- CodeChef/salary.c
+++ CodeChef/salary.c
@@ -1,18 +1,17 @@
 #include<stdio.h>
 int main(){
 	int BS, DA, HRA, salary;
-	printf("Enter the Basic Salary: \n");
+	printf("Enter the Basic Salary: ");
 	scanf("%d", &BS);
 
-	if(BS >= 5000){
-	HRA = 1000;
-	DA = BS/10;
+	if ( BS >= 5000 ) {
+		HRA = 1000;
+		DA = BS/10;
+	} else {
+		HRA = 500;
+		DA = BS/20;
 	}
-	else{
-	HRA = 500;
-	DA = BS/20;
-	}
-	salary = BS+DA+HRA;
+	salary = BS + DA + HRA;
 	printf("Your salary is : Rs.  %d\n", salary);
 	return 0;
 }
