f520fe0aec5d19fcd8a491501d0c4231670c8dbb
deepuguptacp <199deepu@gmail.com>
2018-01-06 21:03:11 +0530

    prashant

---
+++ CodeChef/prashant.c
@@ -0,0 +1,78 @@
+#include<stdio.h>
+#define CREDIT 4 
+int main() {
+    double engineering_mathematics,element_of_electronics_engineering ,engineering_mechanics ,chemical_science,element_of_electronics_engineering_lab,chemical_science_lab, engineering_drawing;
+    double communication_skill, humanity_and_social_science,percentage;
+    printf("this program is coded by PRASHANT THAKUR\n");
+    printf("follow the instuction  below to calculate your percentage in first semester\n");
+
+    printf("enter marks in engineering_mathematics\n");
+    scanf("%lf", &engineering_mathematics);
+
+    printf("enter marks in element_of_electronics_engineering\n");
+    scanf("%lf", &element_of_electronics_engineering);
+
+    printf("enter marks in engineering_mechanics\n");
+    scanf("%lf", &engineering_mechanics);
+
+    printf("enter marks in chemical_science \n");
+    scanf("%lf", &chemical_science);
+
+    printf("enter marks in element_of_electronics_engineering_lab\n ");
+    scanf("%lf", &element_of_electronics_engineering_lab);
+
+
+    printf("enter marks in chemical_science_lab\n ");
+    scanf("%lf",&chemical_science_lab);
+
+    printf("enter marks in communication_skill\n ");
+    scanf("%lf",&communication_skill);
+    printf("enter marks in humanity_and_social_science\n ");
+    scanf("%lf",&humanity_and_social_science);
+    printf("enter marks in engineering_drawing\n");
+    scanf("%lf",&engineering_drawing);
+
+    //percentage is (summation of product of marks and credit)/(sum of credit);
+    percentage=(engineering_mathematics*4 + 
+                engineering_mechanics*4 +
+                element_of_electronics_engineering*4+
+                chemical_science*3+
+                communication_skill*2+ 
+                humanity_and_social_science*2+
+                engineering_drawing*2+
+                chemical_science_lab+
+                element_of_electronics_engineering_lab)/23;
+
+
+    if(percentage<=33){
+        printf("according to your marks entered you have got percent of %lf\n",percentage);
+        printf("sorry you are fail");
+    }
+
+    if(percentage<=60&&percentage>33){
+        printf("according to your marks entered you have got percent of %lf\n",percentage);
+        printf("your performance is average ,work hard");
+    }
+
+    if(percentage<=80&&percentage>60){
+        printf("according to your marks entered you have got percent of %lf\n",percentage);
+        printf("your overall performance is good,your performance suggest that you have fair chance of improvement");
+    }
+
+    if(percentage<=95&&percentage>80){
+        printf("according to your marks entered you have got percent of %lf\n",percentage);
+        printf("your performance is excellent ,keep it up");
+    }
+
+    if(percentage<=100&&percentage>95){
+        printf("according to your marks entered you have got percent of %lf\n",percentage);
+        /*note that when \n is written after " it show fault*/
+        printf("your performance is outstanding, you are genius");
+    }
+    //also note that when more two statement are there in if and if else loop, in each condition then braces must be used otherwise only first line 
+    //will be used and when it comes to secpnd line then compiler think that there is no stement followed with with before if else 
+    //\n must be used for escape not /n.
+    //note here that even if we use else if or only if we get same  output
+
+    return 0;
+}
