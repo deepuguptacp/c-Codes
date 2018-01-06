ec5d53c139b29d5dc13e7ec9fe92b4386ba862c3
deepuguptacp <199deepu@gmail.com>
2018-01-07 00:27:15 +0530

    task

---
+++ Task/sum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main (){
+	double a, b, sum;
+	printf("Enter the value of a: ");
+	scanf("%lf", &a);
+	printf("Enter the value of b: ");
+	scanf("%lf", &b);
+	sum = a + b;
+	printf("sum :%lf\n", sum);
+	return 0;
+}
