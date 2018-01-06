9f0c1d5df980011c16c029a7fbdd8bebd8a0a38e
deepuguptacp <199deepu@gmail.com>
2018-01-07 00:07:55 +0530

    difference

---
+++ CodeChef/difference.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main (){
+	int a, b, difference;
+	printf("Enter the value of a\n");
+	scanf("%d", &a);
+	printf("Enter the value of b\n");
+	scanf("%d", &b);
+	difference = a - b;
+	printf("Your difference is:%d\n", difference);
+	return 0;
+}
