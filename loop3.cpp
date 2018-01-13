51e0e6b7b5d2013d6a25b88b33ee792d3cca0810
deepuguptacp <199deepu@gmail.com>
2018-01-13 17:29:12 +0530

    loop

---
+++ loop3.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+	int i, j, k=5;
+	for(i=1; i<=k; i++)
+	{
+		for(j=i; j<=k; j++)
+		{
+			printf(" ");
+		}
+		for(j=1; j<=(2*i-1); j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
