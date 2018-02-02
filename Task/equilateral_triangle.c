a5a7ee420f17e5a689010511b3df8e6d0c620758
deepuguptacp <199deepu@gmail.com>
2018-02-02 23:42:52 +0530

    change

--- Task/equilateral_triangle.c
+++ Task/equilateral_triangle.c
@@ -4,8 +4,8 @@ int main(){
 	printf("Enter the side length : ");
 	scanf("%lf", &a);
 	// area = (sqrt(3)/4)*a*a
-	area = 0.433 * a; // wrong, do you remember area has dimension of (m*m), [unit dimension]
-	perimeter = 3 * a;
+	area = 0.433 * a * a; // wrong, do you remember area has dimension of (m*m), [unit dimension]
+	perimeter = 3 * a ;
 	printf("Area of Equilateral traingle is : %lf\n", area);
 	printf("Perimeter of Equilateral traingle is : %lf\n", 														Perimeter);
 	return 0;
