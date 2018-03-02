a07dd54918e7c23b12190ad115086b2cf038ddc7
ashu01 <as01hu@gmail.com>
2018-03-03 00:58:22 +0530

    generic code and convention

--- Class work/name.c
+++ Class work/name.c
@@ -2,13 +2,12 @@
 int main(){
     int i;
     char name[5];
-    for(i=0; i<5; i++)
-    {
+    for(i=0; i<5; i++) {
         printf("\nEnter the %d letter of your name : ", i+1);
         scanf("%c", &name[i]);
     }
-    for(i=0; i<5; i++)
-    {
+    for(i=0; i<5; i++) {
         printf("%c\n", name[i]);
     }
+    return 0;
 }
