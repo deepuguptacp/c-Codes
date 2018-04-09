d83ac82ea47f1eb92bb4e0add7bba0aa858016f6
deepuguptacp <199deepu@gmail.com>
2018-04-09 23:19:02 +0530

    class work

--- Class work/Loop.c
+++ Class work/Loop.c
@@ -1,14 +1,19 @@
 #include<stdio.h>
-int main(){
+int main()	
+{
     int i, j, k, l;
-    for(i=1; i<=4; i++) {
-        for(j=4; j>i; j--) {
+    for(i=1; i<=4; i++) 
+	{
+        for(j=4; j>i; j--) 
+		{
             printf(" ");
         }
-        for(k=1; k<=i; k++) {
+        for(k=1; k<=i; k++) 
+		{
             printf("%d", k+i-1);
         }
-        for(l=1; l<i; l++) {
+        for(l=1; l<i; l++)	 
+		{
             printf("%d", i+l-1);
         }
         printf("\n");
